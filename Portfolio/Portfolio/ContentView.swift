48c781733d16f712c84a47929905b5db99caa2d7
Regina Musovirova <musovirova@yahoo.com>
2020-02-01 11:13:39 -0500

    change

---
+++ Portfolio/Portfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Portfolio
+//
+//  Created by Regina Musovirova on 1/30/20.
+//  Copyright © 2020 Regina Musovirova. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
